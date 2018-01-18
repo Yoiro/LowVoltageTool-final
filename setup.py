b4cb0839d26c5cf88f593acf2abc6c38a03f31c0
Alastard <degrevesim@gmail.com>
2018-01-18 21:32:48 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='LowVoltageTool',
+    version='0.4',
+    packages=['app', 'app.models', 'app.wxviews', 'app.wxviews.core', 'app.wxviews.items', 'app.wxviews.panels',
+              'app.wxviews.panels.table', 'app.wxviews.drawers', 'app.wxviews.patterns', 'app.serialization'],
+    url='',
+    license='',
+    author='Simon De Greve',
+    author_email='degrevesim@gmail.com',
+    description=''
+)
