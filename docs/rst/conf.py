24db2c78729bb96b807a3c4ec583ee1c27bc9df4
Yoiro <degrevesim@gmail.com>
2018-02-08 18:13:23 +0100

    update

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -20,6 +20,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(''))
+
+
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +34,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc']
-autodoc_mock_imports = ['anytree', 'numpy', 'scipy', 'wxpython']
+autodoc_mock_imports = ['anytree', 'numpy', 'scipy']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
